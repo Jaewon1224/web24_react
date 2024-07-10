dae707db605c60f86586dc00bf53e763bb68d261
Jaewon <happy417780@gmail.com>
2024-07-10 12:43:51 +0900

    0710 수업

--- src/01/MyCom.js
+++ src/01/MyCom.js
@@ -1,7 +1,11 @@
 import clock from './colock.png' ;
 import './MyCom.css' ;
+import {useState, useEffect} from 'react' ;
 
 function MyCom() {
+  //상태 변수 선언
+  const  [tm, setTm] = useState(new Date());
+
   const mycomDiv = {
     width : '100%',
     display: 'flex',
@@ -9,13 +13,25 @@ function MyCom() {
     alignItems: 'center',
     flexDirection: 'column'
   }
+
+  //컴포넌트 생성시 한번 실행
+  useEffect(()=>{
+    const timer = setInterval(()=>{
+      setTm(new Date());
+    }, 1000);
+
+    return (()=>{
+      clearInterval(timer);
+    });
+  },[]);
+
   return (
     <div style={mycomDiv}>
       <p className='mycomP'>
         <img src={clock} alt='시계' style={{'width':'400px'}} />
       </p>
-      <p className='mycomP'>
-        현재 시간 : {new Date().toLocaleTimeString()}
+      <p className='text-white text-lg font-bold bg-blue-800'>
+        현재 시간 : {tm.toLocaleTimeString()}
       </p>
     </div>
   );
---
+++ src/03/CardMain.js
@@ -0,0 +1,21 @@
+import Card from "./Card";
+import CData from "./CardData.json";
+
+export default function CardMain() {
+  console.log(CData);
+  const tags = CData.map(item => <Card imgSrc={item.imgUrl}
+                                        title={item.title}
+                                         content={item.content}/>
+
+
+  )
+
+  return (
+    <div className="w-10/12 grid grid-cols-1 lg:grid-cols-2 gap-4">
+      {/* <Card imgSrc={imgSrc} 
+            title={title} 
+            content={content} /> */}
+      {tags}
+    </div>
+  )
+}
